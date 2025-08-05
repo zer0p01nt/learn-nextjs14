02fdac941ecd89ddafd6b6a1be9d79d5c585493e
zer0p01nt <1027.bmy@gmail.com>
2025-08-05 18:33:54 +0900

    Feat: prefetch 추가

--- components/movie.jsx
+++ components/movie.jsx
@@ -12,7 +12,9 @@ export default function Movie({ title, id, poster_path }) {
   return (
     <div className={styles.movie}>
       <img src={poster_path} alt={title} onClick={onClick} />
-      <Link href={`/movies/${id}`}>{title}</Link>
+      <Link prefetch href={`/movies/${id}`}>
+        {title}
+      </Link>
     </div>
   );
 }
