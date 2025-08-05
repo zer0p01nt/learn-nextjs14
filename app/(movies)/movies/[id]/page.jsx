f8e0566fefe0d2fb73bb14a3d8ee72190bcabfac
zer0p01nt <1027.bmy@gmail.com>
2025-08-06 01:14:05 +0900

    Fix: params 비동기 처리로 수정

--- app/(movies)/movies/[id]/page.jsx
+++ app/(movies)/movies/[id]/page.jsx
@@ -2,14 +2,16 @@ import { Suspense } from "react";
 import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
-export async function generateMetadata({ params: { id } }) {
+export async function generateMetadata({ params }) {
+  const { id } = await params;
   const movie = await getMovie(id);
   return {
     title: movie.title,
   };
 }
 
-export default async function MovieDetail({ params: { id } }) {
+export default async function MovieDetail({ params }) {
+  const { id } = await params;
   return (
     <div>
       <Suspense fallback={<h1>Loading movie info</h1>}>
