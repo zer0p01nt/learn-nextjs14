7c1c4a11f4d2bbd0fea50214b7df40178a3227b7
zer0p01nt <1027.bmy@gmail.com>
2025-08-05 18:27:42 +0900

    Feat: 동적 metadata 생성

--- app/(movies)/movies/[id]/page.jsx
+++ app/(movies)/movies/[id]/page.jsx
@@ -1,11 +1,17 @@
 import { Suspense } from "react";
-import MovieInfo from "../../../../components/movie-info";
+import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
+
+export async function generateMetadata({ params: { id } }) {
+  const movie = await getMovie(id);
+  return {
+    title: movie.title,
+  };
+}
 
 export default async function MovieDetail({ params: { id } }) {
   return (
     <div>
-      <h3>Movie Detail Page</h3>
       <Suspense fallback={<h1>Loading movie info</h1>}>
         <MovieInfo id={id} />
       </Suspense>
