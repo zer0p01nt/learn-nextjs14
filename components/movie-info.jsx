4ca90427c7bd2b6a8365b54c354cb20a8b784294
zer0p01nt <1027.bmy@gmail.com>
2025-08-05 18:27:26 +0900

    Feat: css 적용, 퍼블리싱

--- components/movie-info.jsx
+++ components/movie-info.jsx
@@ -1,11 +1,28 @@
 import { API_URL } from "../app/(home)/page";
+import styles from "../styles/movie-info.module.css";
 
-async function getMovie(id) {
+export async function getMovie(id) {
   const response = await fetch(`${API_URL}/${id}`);
   return response.json();
 }
 
-export default async function MovieInfo(id) {
+export default async function MovieInfo({ id }) {
   const movie = await getMovie(id);
-  return <h6>{JSON.stringify(movie)}</h6>;
+  return (
+    <div className={styles.container}>
+      <img
+        src={movie.poster_path}
+        className={styles.poster}
+        alt={movie.title}
+      />
+      <div className={styles.info}>
+        <h1 className={styles.title}>{movie.title}</h1>
+        <h3>⭐️ {movie.vote_average.toFixed(1)}</h3>
+        <p>{movie.overview}</p>
+        <a href={movie.homepage} target={"_blank"}>
+          Homepage &rarr;
+        </a>
+      </div>
+    </div>
+  );
 }
--- components/movie-videos.jsx
+++ components/movie-videos.jsx
@@ -1,11 +1,24 @@
 import { API_URL } from "../app/(home)/page";
+import styles from "../styles/movie-videos.module.css";
 
 async function getVideos(id) {
   const response = await fetch(`${API_URL}/${id}/videos`);
   return response.json();
 }
 
-export default async function MovieVideos(id) {
+export default async function MovieVideos({ id }) {
   const videos = await getVideos(id);
-  return <h6>{JSON.stringify(videos)}</h6>;
+  return (
+    <div className={styles.container}>
+      {videos.map((video) => (
+        <iframe
+          key={video.id}
+          src={`https://youtube.com/embed/${video.key}`}
+          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+          allowFullScreen
+          title={video.name}
+        />
+      ))}
+    </div>
+  );
 }
